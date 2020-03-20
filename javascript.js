90bb71fc48b215dc69c3e36321e0b477dc7dd1f2
Dylan Ambrose <djambro44@gmail.com>
2020-03-19 19:01:21 -0700

    added timer

--- javascript.js
+++ javascript.js
@@ -51,6 +51,73 @@ $(document).ready(function(){
             console.log(word);
         }
         showLyrics();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  $("#start").on("click", startTimer);
+
+
+
+        var time = 0; 
+        var clockRunning = false;
+
+
+        //timer reset
+        function reset() {
+
+          time = 0;
+          $("#display").text("02:00");
+        }
+
+        //start timer function
+
+        function startTimer(){
+          if(!clockRunning){
+            intervalId = setInterval(count, 1000);
+            clockRunning = true;
+          }
+        }
+
+        //stops timer function
+
+        function stopTimer() {
+
+          clearInterval(intervalId);
+          clockRunning = false;
+        }
+
+        function count() {
+          time--;
+          var converted = timeConverter(time);
+          console.log(converted);
+        
+          $("#display").text(converted);
+        }
+        function timeConverter(t) {
+        
+          var minutes = Math.floor(t / 60);
+          var seconds = t - (minutes * 60);
+        
+          if (seconds < 10) {
+            seconds = "0" + seconds;
+          }
+        
+          if (minutes === 0) {
+            minutes = "00";
+          }
+          else if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+        
+          return minutes + ":" + seconds;
+        }
+
+        function TimerExpireGameOver(){
+          if (time == 0){
+            //link to Game Over page
+          }
+        }
+
 
 
 
