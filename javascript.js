f87c82dca300e09383a4fdd9516c2d5576d44071
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-03-24 12:59:21 -0700

    found the api info for each song and fixed the progress bar but I cannot figure out how to make autoplay work

--- javascript.js
+++ javascript.js
@@ -1,48 +1,4 @@
 $(document).ready(function() {
-  // function searchSong() {
-
-  //     var song = $(this).attr("data-name");
-  //     var queryURL = "https://api.spotify.com/v1/search?q=guns%20n'%20roses&type=welcome%20to%20the%20jungle%2C%20guns%20n'%20roses&market=United%20States&limit=1";
-
-  //     $.ajax({
-  //       url: queryURL,
-  //       method: "GET"
-  //     }).then(function(response) {
-  //       $("#lyrics").text(JSON.stringify(response));
-  //       console.log(response);
-  //     });
-  //   }
-
-  //   searchSong();
-
-  var settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://deezerdevs-deezer.p.rapidapi.com/track/518458092",
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-      "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
-    }
-  };
-
-  $.ajax(settings).done(function(response) {
-    console.log(response);
-    var player = 0;
-
-    $("#playAgain").on("click", function() {
-      if (player === 0) {
-        $("#musicPlayer").append(
-          `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-3.dzcdn.net/stream/c-3e477a6671b162c3d05dac2cfffbe2e7-4.mp3" type="audio/mpeg"></video>`
-        );
-        // $("#musicPlayer").hide();
-        $("#musicPlayer");
-        player++;
-      }
-      console.log(response.preview);
-    });
-  });
-
   var text = $(".word")
       .text()
       .split(""),
@@ -63,6 +19,22 @@ $(document).ready(function() {
       }, i * vel);
     });
   }
+
+  // function searchSong() {
+
+  //     var song = $(this).attr("data-name");
+  //     var queryURL = "https://api.spotify.com/v1/search?q=guns%20n'%20roses&type=welcome%20to%20the%20jungle%2C%20guns%20n'%20roses&market=United%20States&limit=1";
+
+  //     $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     }).then(function(response) {
+  //       $("#lyrics").text(JSON.stringify(response));
+  //       console.log(response);
+  //     });
+  //   }
+
+  //   searchSong();
 
   var allLyrics = "";
 
@@ -185,11 +157,16 @@ $(document).ready(function() {
   };
 });
 
-var timeleft = 10;
+// ------------ PROGRESS BAR -------------------
+
+var timeleft = 2;
 var startGameTimer = setInterval(function() {
   if (timeleft === 0) {
     $(".playingTheGame").show();
     $(".beforeGame").hide();
+    $("#musicPlayer").append(
+      `<audio controls="" autoplay name="media"><source src="https://cdns-preview-c.dzcdn.net/stream/c-ce054ab331a0bf0fece4cfc1fb7a72b6-8.mp3" type="audio/mpeg"></audio>`
+    );
 
     clearInterval(startGameTimer);
   }
@@ -197,3 +174,190 @@ var startGameTimer = setInterval(function() {
   document.getElementById("progressBar").value = 10 - timeleft;
   timeleft -= 1;
 }, 1000);
+
+// ------- ME TRYING TO GET IT TO WORK ----------
+
+// var timeleft = 2;
+// var startGameTimer = setInterval(function() {
+//   if (timeleft === 0) {
+//     $(".playingTheGame").show();
+//     $(".beforeGame").hide();
+
+//     clearInterval(startGameTimer);
+//   }
+
+//   document.getElementById("progressBar").value = 10 - timeleft;
+//   timeleft -= 1;
+// }, 1000);
+
+// var settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://deezerdevs-deezer.p.rapidapi.com/track/518458092",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+//     "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+//   }
+// };
+
+// $.ajax(settings).done(function(response) {
+//   console.log(response);
+//   var player = 0;
+
+//   function musicPlayer() {
+//     if ($(".beforeGame").css("display") == "none") {
+//       $("#musicPlayer").append(
+//         `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-3.dzcdn.net/stream/c-3e477a6671b162c3d05dac2cfffbe2e7-4.mp3" type="audio/mpeg"></video>`
+//       );
+//       player++;
+//     }
+//     console.log(response.preview);
+//   }
+//   musicPlayer();
+// });
+
+// __________TEST API _______
+
+// var settings = {
+//   async: true,
+//   crossDomain: true,
+//   url: "https://deezerdevs-deezer.p.rapidapi.com/track/518458092",
+//   method: "GET",
+//   headers: {
+//     "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+//     "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+//   }
+// };
+
+// $.ajax(settings).done(function(response) {
+//   console.log(response);
+//   var player = 0;
+
+//   $("#playAgain").on("click", function() {
+//     if (player === 0) {
+//       $("#musicPlayer").append(
+//         `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-3.dzcdn.net/stream/c-3e477a6671b162c3d05dac2cfffbe2e7-4.mp3" type="audio/mpeg"></video>`
+//       );
+//       // $("#musicPlayer").hide();
+//       $("#musicPlayer");
+//       player++;
+//     }
+//     console.log(response.preview);
+//   });
+// });
+
+// ------------- LORED API DATA --------------------
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/track/70403437",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+  }
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+  var player = 0;
+
+  $("#playAgain").on("click", function() {
+    if (player === 0) {
+      $("#musicPlayer").append(
+        `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-9.dzcdn.net/stream/c-9f7a463d7c928f017d69aaa7f2b810e8-4.mp3" type="audio/mpeg"></video>`
+      );
+      // $("#musicPlayer").hide();
+      player++;
+    }
+    console.log(response.preview);
+  });
+});
+
+// ------------- THE CRANBERRIES API DATA --------------------
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/track/5652656",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+  }
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+  var player = 0;
+
+  $("#playAgain").on("click", function() {
+    if (player === 0) {
+      $("#musicPlayer").append(
+        `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-3.dzcdn.net/stream/c-3f9241907a470bd4a29d2cf137dfc870-8.mp3" type="audio/mpeg"></video>`
+      );
+      // $("#musicPlayer").hide();
+      player++;
+    }
+    console.log(response.preview);
+  });
+});
+
+// ------------- Cyndi Lauper API DATA --------------------
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/track/608756",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+  }
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+  var player = 0;
+
+  $("#playAgain").on("click", function() {
+    if (player === 0) {
+      $("#musicPlayer").append(
+        `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-a.dzcdn.net/stream/c-adf8199f340d68ad579ebe31321225c8-4.mp3" type="audio/mpeg"></video>`
+      );
+      // $("#musicPlayer").hide();
+      player++;
+    }
+    console.log(response.preview);
+  });
+});
+
+// ------------- B-52s API DATA --------------------
+
+var settings = {
+  async: true,
+  crossDomain: true,
+  url: "https://deezerdevs-deezer.p.rapidapi.com/track/882083172",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    "x-rapidapi-key": "86c58d10b1msh8d0d204efb10993p118dbfjsn1cf0dff99932"
+  }
+};
+
+$.ajax(settings).done(function(response) {
+  console.log(response);
+  var player = 0;
+
+  $("#playAgain").on("click", function() {
+    if (player === 0) {
+      $("#musicPlayer").append(
+        `<video controls="" autoplay="" name="media"><source src="https://cdns-preview-c.dzcdn.net/stream/c-ce054ab331a0bf0fece4cfc1fb7a72b6-8.mp3" type="audio/mpeg"></video>`
+      );
+      // $("#musicPlayer").hide();
+      player++;
+    }
+    console.log(response.preview);
+  });
+});
